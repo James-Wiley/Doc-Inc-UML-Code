088087fcf884280d35eabd6154d571e7089f1957
James-Wiley <james.wiley@live.com>
2026-02-19 15:34:17 -0600

    clang-format and fix User bracket placement

--- User.cpp
+++ User.cpp
@@ -1,16 +1,19 @@
 #include "User.h"
 
-void User::login() {
+void User::login() 
+{
 	// TODO - implement User::login
 	throw "Not yet implemented";
 }
 
-void User::viewStatements() {
+void User::viewStatements() 
+{
 	// TODO - implement User::viewStatements
 	throw "Not yet implemented";
 }
 
-void User::updatePreferences() {
+void User::updatePreferences() 
+{
 	// TODO - implement User::updatePreferences
 	throw "Not yet implemented";
 }
